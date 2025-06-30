fb703ee765a40081b1dc16bbb04c50212ebac79e
vikramiyeroncemore <95141250+vikramiyeroncemore@users.noreply.github.com>
2025-06-30 12:31:12 +0000

    Deploying to gh-pages from @ vikramiyeroncemore/vikramiyeroncemore.github.io@64e4ac0a829cbfe744e1c2a309da589623047c7d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "About",
+    id: "nav-landing",
+    title: "Landing",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
